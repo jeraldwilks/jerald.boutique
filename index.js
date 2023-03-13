e2df63f2a8709cbc621451a080640a9ccab04213
Jerald Wilks <jerald@wilks.pro>
2023-03-13 11:14:04 -0600

    update port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 const app = express();
-const PORT = 443;
+const PORT = process.env.PORT || 443;
 const __dirname = path.resolve();
 app.use(express.json());
 
