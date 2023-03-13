11f022f84a4fb42701fccd999a51e75d92de2fb5
Jerald Wilks <jerald@wilks.pro>
2023-03-13 11:04:01 -0600

    second

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
-
+import path from "path";
 const app = express();
 const PORT = 443;
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/readme.html");
+});
+console.log(__dirname);
