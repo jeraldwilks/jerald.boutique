848ab11cefced78e11a18e28c38af1f8b576b3ba
Jerald Wilks <jerald@wilks.pro>
2023-03-13 20:36:05 -0600

    removed itemRouter comments

--- itemRouter.js
+++ itemRouter.js
@@ -22,14 +22,3 @@ itemRouter.get("/", async (req, res) => {
     res.status(500).send(error);
   }
 });
-
-// app.post("/add_user", async (request, response) => {
-//     const user = new userModel(request.body);
-
-//     try {
-//       await user.save();
-//       response.send(user);
-//     } catch (error) {
-//       response.status(500).send(error);
-//     }
-// });
