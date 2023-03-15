eb4c5db5263256ba346dfdfa344c193e9b0290b9
Jerald Wilks <jerald@wilks.pro>
2023-03-14 18:37:02 -0600

    removed seperate mongoConnect.js

--- server.js
+++ server.js
@@ -2,23 +2,31 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { db } from "./mongoConnect.js";
+// import { db } from "./mongoConnect.js";
 import { itemRouter } from "./itemRouter.js";
 import { itemModel } from "./models.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const dirname = path.resolve();
+
+mongoose.connect(process.env.MONGO_URL);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", () => {
+  console.log("Connected to MongoDB successfully");
+});
+
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/readme.html");
+  response.sendFile(dirname + "/readme.html");
 });
 app.get("/favicon.ico", (request, response) => {
-  response.sendFile(__dirname + "/favicon.ico");
+  response.sendFile(dirname + "/favicon.ico");
 });
 
 app.use("/items", itemRouter);
