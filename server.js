b5d42a62492d85fc5cf6f6f078880e1b07a9a8bc
Jerald Wilks <jerald@wilks.pro>
2023-03-21 17:21:57 -0600

    changed swagger back to api-docs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "jerald.boutique",
-      version: "0.3.1",
+      version: "0.4.0",
       description: "Project 1 - Simple Proof of Concept for Sales & Inventory",
     },
   },
@@ -38,15 +38,15 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// app.get("/", (request, response) => {
-//   response.sendFile(dirname + "/readme.html");
-// });
-// app.get("/favicon.ico", (request, response) => {
-//   response.sendFile(dirname + "/favicon.ico");
-// });
-// app.get("/Project1.jpg", (request, response) => {
-//   response.sendFile(dirname + "/Project1.jpg");
-// });
-app.use("/", SwaggerUI.serve, SwaggerUI.setup(specs));
+app.get("/", (request, response) => {
+  response.sendFile(dirname + "/readme.html");
+});
+app.get("/favicon.ico", (request, response) => {
+  response.sendFile(dirname + "/favicon.ico");
+});
+app.get("/Project1.jpg", (request, response) => {
+  response.sendFile(dirname + "/Project1.jpg");
+});
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(specs));
 app.use("/items", itemRouter);
 app.use("/sales", saleRouter);
